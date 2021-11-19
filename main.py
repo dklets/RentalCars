2db42adafbcdabd1027259df8e1d3c92bf16df20
dklets <d.m.klets@gmail.com>
2021-11-19 22:53:27 +0200

    Commit v.19.11.22:53

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from typing import Callable
+from sqlalchemy.orm import relationship
 
 
 class MySQLAlchemy(SQLAlchemy):
     Column: Callable  # Use the typing to tell the IDE what the type is
     String: Callable
     Integer: Callable
-    Text: Callable
+    Float: Callable
     DateTime: Callable
+    ForeignKey: Callable
 
 
 rental_cars = Flask(__name__)
@@ -18,15 +20,36 @@ rental_cars.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = MySQLAlchemy(rental_cars)
 
 
-class Orders(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+class Cars(db.Model):
+    number = db.Column(db.String(30), primary_key=True)
+    description = db.Column(db.String(90), nullable=False)
+    rental_cost = db.Column(db.Float(), nullable=False)
+    children = relationship("Child")
 
     def __repr__(self):
-        return '<Order %r>' % self.id
+        return '<Car %r>' % self.number
+
+
+class Clients(db.Model):
+    passport_number = db.Column(db.String(30), primary_key=True)
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    registration_date = db.Column(db.DateTime, default=datetime.utcnow)
+    children = relationship("Child")
+
+    def __repr__(self):
+        return '<Client %r>' % self.passport_number
+
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    add_date = db.Column(db.DateTime, default=datetime.utcnow)
+    rental_time = db.Column(db.Integer, nullable=False)
+    client_passport_number = db.Column(db.String(30), db.ForeignKey('clients.passport_number'))
+    car_number = db.Column(db.String(30), db.ForeignKey('cars.number'))
+
+    def __repr__(self):
+        return '<Order rental time %r>' % self.id
 
 
 @rental_cars.route('/')
@@ -34,6 +57,7 @@ def index():
     return render_template("index.html")
 
 
+"""
 @rental_cars.route('/create-order', methods=['POST', 'GET'])
 def create_order():
     if request.method == 'POST':
@@ -51,6 +75,7 @@ def create_order():
             return "При добавлении заказа произошла ошибка"
     else:
         return render_template("create-order.html")
+"""
 
 
 @rental_cars.route('/department')
