fdfa9f3bef6cbdb57fbb6626b91113d045877db3
dklets <d.m.klets@gmail.com>
2021-11-20 15:26:47 +0200

    Commit v.20.11.15:26

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from typing import Callable
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 
 
 class MySQLAlchemy(SQLAlchemy):
@@ -21,35 +21,37 @@ db = MySQLAlchemy(rental_cars)
 
 
 class Cars(db.Model):
-    number = db.Column(db.String(30), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(30), nullable=False)
     description = db.Column(db.String(90), nullable=False)
     rental_cost = db.Column(db.Float(), nullable=False)
-    children = relationship("Child")
+    # children = relationship("Child")
 
     def __repr__(self):
-        return '<Car %r>' % self.number
+        return f'<cars {self.id}>'
 
 
 class Clients(db.Model):
-    passport_number = db.Column(db.String(30), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    passport_number = db.Column(db.String(30), nullable=False)
     first_name = db.Column(db.String(45), nullable=False)
     last_name = db.Column(db.String(45), nullable=False)
-    registration_date = db.Column(db.DateTime, default=datetime.utcnow)
-    children = relationship("Child")
+    registration_date = db.Column(db.String(45), nullable=False)
+    # children = relationship("Child")
 
     def __repr__(self):
-        return '<Client %r>' % self.passport_number
+        return f'<clients {self.id}>'
 
 
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     add_date = db.Column(db.DateTime, default=datetime.utcnow)
     rental_time = db.Column(db.Integer, nullable=False)
-    client_passport_number = db.Column(db.String(30), db.ForeignKey('clients.passport_number'))
-    car_number = db.Column(db.String(30), db.ForeignKey('cars.number'))
+    client_id = db.Column(db.String(30), db.ForeignKey('clients.id'))
+    car_id = db.Column(db.String(30), db.ForeignKey('cars.id'))
 
     def __repr__(self):
-        return '<Order rental time %r>' % self.id
+        return f'<orders {self.id}>'
 
 
 @rental_cars.route('/')
@@ -57,25 +59,39 @@ def index():
     return render_template("index.html")
 
 
-"""
-@rental_cars.route('/create-order', methods=['POST', 'GET'])
-def create_order():
+@rental_cars.route('/add-client', methods=['POST', 'GET'])
+def add_client():
     if request.method == 'POST':
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-
-        order = Orders(title=title, intro=intro, text=text)
-
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        passport_number = request.form['passport_number']
+        registration_date = request.form['registration_date']
+        client = Clients(first_name=first_name, last_name=last_name, passport_number=passport_number, registration_date=registration_date)
         try:
-            db.session.add(order)
+            db.session.add(client)
             db.session.commit()
             return redirect('/')
         except:
-            return "При добавлении заказа произошла ошибка"
+            return "Error: add client"
     else:
-        return render_template("create-order.html")
-"""
+        return render_template("add-client.html")
+
+
+@rental_cars.route('/add-car', methods=['POST', 'GET'])
+def add_car():
+    if request.method == 'POST':
+        description = request.form['description']
+        number = request.form['number']
+        rental_cost = request.form['rental_cost']
+        car = Cars(description=description, number=number, rental_cost=rental_cost)
+        try:
+            db.session.add(car)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error: add car"
+    else:
+        return render_template("add-car.html")
 
 
 @rental_cars.route('/department')
