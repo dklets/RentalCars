4cf41b66ad7fe626aa629d415429e09913081697
dklets <d.m.klets@gmail.com>
2022-01-03 00:02:52 +0200

    Commit v.03.01.00:02

--- rest/rest_cars.py
+++ rest/rest_cars.py
@@ -1,38 +1,12 @@
 import sqlite3
 from flask import Flask, request, jsonify, g
 from flask_sqlalchemy import SQLAlchemy
-from typing import Callable
-
-
-class MySQLAlchemy(SQLAlchemy):
-    Column: Callable  # Use the typing to tell the IDE what the type is
-    String: Callable
-    Integer: Callable
-    Float: Callable
-    DateTime: Callable
-    ForeignKey: Callable
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rental_cars.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = MySQLAlchemy(app)
-
-
-# app = Flask(__name__)
-# db = sqlite3.connect("db.sqlite3")
-# cursor = db.cursor()
-# cursor.execute("""
-#     create table if not exists posts (
-#         id integer primary key,
-#         name varchar(30),
-#         post varchar(140)
-#     )
-# """)
-# cursor.close()
-# db.close()
-
-
+db = SQLAlchemy(app)
 URL = '/cars-list'
 
 
@@ -47,7 +21,7 @@ def to_dict(id, number, description, rental_cost):
 
 @app.before_request
 def init_db():
-    g.db = sqlite3.connect('app.db')
+    g.db = sqlite3.connect('/home/dklets/Projects/RentalCars/rental_cars.db')
 
 
 @app.teardown_request
@@ -55,8 +29,18 @@ def close_db(exc):
     g.db.close()
 
 
+@app.route(URL, methods=["GET"])
+def display_cars():
+    cursor = g.db.cursor()
+    cursor.execute("select id, number, description, rental_cost from cars")
+    cars = cursor.fetchall()
+    cursor.close()
+    return jsonify([to_dict(*car) for car in cars])
+    # curl http://127.0.0.1:5000/cars-list -v
+
+
 @app.route(URL, methods=["POST"])
-def create():
+def add_car():
     number = request.json.get('number', '')
     description = request.json.get('description', '')
     rental_cost = request.json.get('rental_cost', '')
@@ -69,58 +53,43 @@ def create():
     cursor.close()
     g.db.commit()
     return jsonify(to_dict(id, number, description, rental_cost)), 201
+    # curl -X POST http://127.0.0.1:5000/cars-list -H "Content-Type: application/json" -d '{"number": "AX9756XA", "description": "Boulevar", "rental_cost": "80"}' -v
 
 
-@app.route(URL, methods=["GET"])
-def read_all():
+@app.route(f"{URL}/<id>", methods=["PUT"])
+def edit_car(id):
     cursor = g.db.cursor()
-    cursor.execute("select id, number, description, rental_cost from cars")
-    cars = cursor.fetchall()
+    cursor.execute("select number,description,rental_cost from cars where id=?", (id,))
+    car_db = cursor.fetchone()
     cursor.close()
-    return jsonify([to_dict(*cars) for car in cars])
+    if car_db is None:
+        return jsonify({"error": "Car not found"}), 404
+
+    number, description, rental_cost = car_db
+    number = request.json.get("number", number)
+    description = request.json.get("description", description)
+    rental_cost = request.json.get("rental_cost", rental_cost)
+    cursor = g.db.cursor()
+    cursor.execute("update cars set number=?, description=?, rental_cost=? where id=?", (number, description, rental_cost, id))
+    cursor.close()
+    g.db.commit()
+    g.db.close()
+    return jsonify(to_dict(id, number, description, rental_cost))
+    # curl -X PUT http://127.0.0.1:5000/cars-list/11 -H "Content-Type: application/json" -d '{"number": "AX71623", "description": "109EvilG", "rental_cost": "11"}' -v
 
 
-# @app.route(f"{URL}/<id_>", methods=["GET"])
-# def read(id_):
-#     cursor = g.db.cursor()
-#     cursor.execute("select id, name, post from posts where id=?", (id_,))
-#     post = cursor.fetchone()
-#     cursor.close()
-#     if post is None:
-#         return jsonify({"error": "Post not found"}), 404
-#     return jsonify(to_dict(*post))
-
-
-# @app.route(f"{URL}/<id_>", methods=["PUT"])
-# def update(id_):
-#     cursor = g.db.cursor()
-#     cursor.execute("select name, post from posts where id=?", (id_,))
-#     post_db = cursor.fetchone()
-#     cursor.close()
-#     if post_db is None:
-#         return jsonify({"error": "Post not found"}), 404
-#     name, post = post_db
-#     name = request.json.get("name", name)
-#     post = request.json.get("post", post)
-#     cursor = g.db.cursor()
-#     cursor.execute("update posts set name=?, post=? where id=?", (name, post, id_))
-#     cursor.close()
-#     g.db.commit()
-#     g.db.close()
-#     return jsonify(to_dict(id_, name, post))
-
-
-# @app.route(f"{URL}/<id_>", methods=["DELETE"])
-# def delete(id_):
-#     cursor = g.db.cursor()
-#     cursor.execute("delete from posts where id=?", (id_,))
-#     is_deleted = cursor.rowcount
-#     cursor.close()
-#     g.db.commit()
-#     g.db.close()
-#     if not is_deleted:
-#         return jsonify({"error": "Post not found"}), 404
-#     return "", 204
+@app.route(f"{URL}/<id>", methods=["DELETE"])
+def remove_car(id):
+    cursor = g.db.cursor()
+    cursor.execute("delete from cars where id=?", (id,))
+    is_deleted = cursor.rowcount
+    cursor.close()
+    g.db.commit()
+    g.db.close()
+    if not is_deleted:
+        return jsonify({"error": "Car not found"}), 404
+    return "", 204
+    # curl -X DELETE http://127.0.0.1:5000/cars-list/11 -v
 
 
 if __name__ == '__main__':
